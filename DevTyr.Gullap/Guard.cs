c845575325624793d565da0226a3077ecb86e1e7
Ilker Cetinkaya <ilker@ilker.de>
2013-08-05 20:35:45 +0200

    Support providing exception messages for Guard.NotNull()

--- DevTyr.Gullap/Guard.cs
+++ DevTyr.Gullap/Guard.cs
@@ -4,10 +4,10 @@ namespace DevTyr.Gullap
 {
 	public static class Guard
 	{
-		public static void NotNull (object obj, string argumentName)
+		public static void NotNull (object obj, string argumentName, string message = null)
 		{
 			if (obj == null)
-				throw new ArgumentNullException(argumentName);
+				throw new ArgumentNullException(argumentName, message);
 		}
 
 		public static void NotNullOrEmpty (string obj, string argumentName)
