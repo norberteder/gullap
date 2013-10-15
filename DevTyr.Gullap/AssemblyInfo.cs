15e24dea0e83553154fa1641966b05ecf10768e1
Norbert Eder <wpfnerd+github@gmail.com>
2013-10-15 19:22:31 +0200

    One global assembly info for all projects

--- DevTyr.Gullap/AssemblyInfo.cs
+++ DevTyr.Gullap/AssemblyInfo.cs
@@ -5,22 +5,3 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("DevTyr.Gullap")]
 [assembly: AssemblyDescription("Static website generator library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DevTyr (Norbert Eder)")]
-[assembly: AssemblyProduct("Gullap")]
-[assembly: AssemblyCopyright("(c)2013 Norbert Eder")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.1.0")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
---
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DevTyr (Norbert Eder)")]
+[assembly: AssemblyProduct("Gullap")]
+[assembly: AssemblyCopyright("(c)2013 Norbert Eder")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.1.0")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
